28f374bdf98b26a82f68f561bd19afc78fcfd23f
Akula <alexgritskevich@gmail.com>
2026-08-05 17:09:03 +0300

    Create user_settings.py

---
+++ user_settings.py
@@ -0,0 +1,10 @@
+import sys
+try:
+    import starlette.middleware.gzip as st_gzip
+    orig_init = st_gzip.GZipResponder.__init__
+    def patched_init(self, *args, **kwargs):
+        kwargs.setdefault('thread_minimum_size', 1024 * 1024)
+        return orig_init(self, *args, **kwargs)
+    st_gzip.GZipResponder.__init__ = patched_init
+except Exception:
+    pass
